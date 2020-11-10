29898d4fba02b928e38ec13a989da935a1ffc3dd
Cocei Tiberiu <tiberiu.cocei@gmail.com>
2020-11-10 08:40:49 +0200

    TDD - Scraper

---
+++ Python/web_scraper/real_estates.py
@@ -0,0 +1,6 @@
+real_estates = {
+  'tutorial_url': 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia',
+  'static_url': 'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops',
+  'url3': 'scrapper3',
+  'url4': 'scrapper4'
+}
