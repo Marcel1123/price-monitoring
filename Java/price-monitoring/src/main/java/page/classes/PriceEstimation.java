a8a05e3c4a5a0b175f9a4bb252faa85fec2b3a66
Marcel1123 <48123341+Marcel1123@users.noreply.github.com>
2021-01-04 16:17:06 +0200

    algo integration

--- Java/price-monitoring/src/main/java/page/classes/PriceEstimation.java
+++ Java/price-monitoring/src/main/java/page/classes/PriceEstimation.java
@@ -1,21 +1,19 @@
 package page.classes;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import entities.ProductEntity;
+import entities.LocationEntity;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
+import repositories.location.LocationRepository;
+import util.algo.PriceEstimationAlgorithm;
 import util.models.EstimatePriceModel;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @Named
@@ -23,29 +21,39 @@ import java.util.Map;
 @Getter
 @Setter
 public class PriceEstimation {
-    private EstimatePriceModel productEntity;
+    private EstimatePriceModel estimatePriceModel;
+    private int estimatedPrice;
+    private String locationCompleteName;
+
+    @Inject
+    @Getter(AccessLevel.PRIVATE)
+    @Setter(AccessLevel.PRIVATE)
+    private LocationRepository locationRepository;
 
     @PostConstruct
     @SneakyThrows
     public void init(){
         Map<String, Object> parameterValue = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
-        productEntity = (EstimatePriceModel) parameterValue.get("product");
+        estimatePriceModel = (EstimatePriceModel) parameterValue.get("product");
 
-        URL apiUrl = new URL("http://localhost:8000/predict-new-product");
-        HttpURLConnection connection = (HttpURLConnection)apiUrl.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", "application/json; utf-8");
-        connection.setRequestProperty("Accept", "application/json");
-        connection.setConnectTimeout(3000);
-        connection.setReadTimeout(3000);
-        connection.setDoOutput(true);
+        PriceEstimationAlgorithm algorithm = new PriceEstimationAlgorithm();
+        algorithm.makeAPICall(estimatePriceModel);
+        estimatedPrice = algorithm.getApiResponseModel().getMessage();
 
-        OutputStream os = connection.getOutputStream();
-        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        byte[] input = objectWriter.writeValueAsString(productEntity).getBytes(StandardCharsets.UTF_8);
-        os.write(input, 0, input.length);
+        buildLocationName();
+    }
 
-        int responseCode = connection.getResponseCode();
-        System.out.println(connection);
+    private void buildLocationName(){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        LocationEntity locationEntity = locationRepository.getById(estimatePriceModel.getLocation_id());
+
+        stringBuilder.append(locationEntity.getAddress());
+        stringBuilder.append(", ");
+        stringBuilder.append(locationEntity.getCity().getName());
+        stringBuilder.append(", ");
+        stringBuilder.append(locationEntity.getCity().getCountry());
+
+        locationCompleteName = stringBuilder.toString();
     }
 }
